34c24fbbf9d3f74cedea22e80b93853b40d11b89
unknown <abudaudhossainsumon@gmail.com>
2022-07-31 23:08:15 +0600

    add commernt

--- routes/api.js
+++ routes/api.js
@@ -12,17 +12,17 @@ router.get("/", (req, res) => {
     res.send("api")
 });
 
-router.post('/newAccount', account.createAccount) // create new account
-router.post("/numberValidation", account.numberValidation) // number validation
-router.post("/login", accountAuth.login); // login validation
+router.post('/newAccount', account.createAccount) // create new account public routes
+router.post("/numberValidation", account.numberValidation) // number validation public routes
+router.post("/login", accountAuth.login); // login validation public routes
 
-router.post("/createPost", validUserRequestMiddleware, services.addNewPost);
-router.post("/updatePost", validUserRequestMiddleware, services.updatePost);
-router.post("/deletePost", validUserRequestMiddleware, services.deletePost);
+router.post("/createPost", validUserRequestMiddleware, services.addNewPost); // protected
+router.post("/updatePost", validUserRequestMiddleware, services.updatePost); // protected
+router.post("/deletePost", validUserRequestMiddleware, services.deletePost); // protected
 
-router.get("/myPost", validUserRequestMiddleware, services.myPost);
-router.get("/post/:token", validUserRequestMiddleware, services.post);
-router.get("/allPost", services.allPost);
-router.get("/category/:categoryName", services.getPostByCategory);
+router.get("/myPost", validUserRequestMiddleware, services.myPost); // protected
+router.get("/post/:token", validUserRequestMiddleware, services.post); // protected
+router.get("/allPost", services.allPost); //public routes
+router.get("/category/:categoryName", services.getPostByCategory); // public routes
 
 module.exports = router;
